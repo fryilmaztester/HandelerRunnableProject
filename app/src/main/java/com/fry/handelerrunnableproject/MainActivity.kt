4a89d6fed99a4b11c62eaf84fc2b95a4802564b6
Fatih YILMAZ <fatih.yilmaz@foreks.com>
2021-07-22 00:37:55 +0300

    Kronometer

--- app/src/main/java/com/fry/handelerrunnableproject/MainActivity.kt
+++ app/src/main/java/com/fry/handelerrunnableproject/MainActivity.kt
@@ -2,10 +2,41 @@ package com.fry.handelerrunnableproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var number = 0
+    var runnable : Runnable = Runnable{ }
+    var handeler : Handler = Handler()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun start(view:View){
+
+      //  number = 0
+
+        runnable = object : Runnable{
+
+            override fun run() {
+               number = number + 1
+                textView.text = "Sayaç: ${number}"
+                handeler.postDelayed(runnable,1000)
+            }
+
+        }
+        handeler.post(runnable)
+
+    }
+
+    fun stop(view:View){
+        handeler.removeCallbacks(runnable)
+        number = 0
+        textView.text = "Sayaç: 0"
     }
 }
